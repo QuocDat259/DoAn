f2326719de21930b558ed58d5a552fb9bd637b22
QuocDat259 <cunktvn06@gmail.com>
2023-11-12 00:25:33 +0700

    Admin tạo tài khoản cho Nhân viên

--- NhaKhoa/Areas/Admin/Controllers/QLNhanVienController.cs
+++ NhaKhoa/Areas/Admin/Controllers/QLNhanVienController.cs
@@ -1,48 +1,165 @@
-﻿using Microsoft.AspNet.Identity;
-using NhaKhoa.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using PagedList;
-
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using NhaKhoa.Models;
+using System.Threading.Tasks;
 
 namespace NhaKhoa.Areas.Admin.Controllers
 {
-    [Authorize(Roles = "Admin")]
     public class QLNhanVienController : Controller
     {
         private Nhakhoa db = new Nhakhoa();
-        // GET: Admin/QLNhanVien
-        // GET: Admin/QLNhanVien
-        public ActionResult Index(int? page, string searchString)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public QLNhanVienController()
         {
-            var roleName = "NhanVien";
-            var userRole = db.AspNetRoles.SingleOrDefault(r => r.Name == roleName);
-
-            if (userRole != null)
-            {
-                var users = userRole.AspNetUsers
-                    .Where(u => string.IsNullOrEmpty(searchString) || u.FullName.ToLower().Contains(searchString.ToLower()))
-                    .ToList();
-
-                int pageSize = 5;
-                int pageNumber = page ?? 1;
-
-                var pagedUsers = users.ToPagedList(pageNumber, pageSize);
-
-                ViewBag.SearchString = searchString;
-
-                return View(pagedUsers);
-            }
-
-            return View(new List<AspNetUser>());
+            _userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
         }
-        [HttpPost]
-        public ActionResult TaoTaiKhoanNV()
+
+        // GET: Admin/QLNhanVien
+        public ActionResult Index()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            // Lấy danh sách người dùng thuộc vai trò "NhanVien"
+            var nhanVienUsers = userManager.Users.Where(u => u.Roles.Any(r => r.RoleId == "3")).ToList();
+
+            return View(nhanVienUsers);
+        }
+
+        // GET: Admin/QLNhanVien/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // GET: Admin/QLNhanVien/Create
+        public ActionResult Create()
         {
             return View();
+        }
+
+        // POST: Admin/QLNhanVien/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber,
+                    UserName = model.UserName,
+                    GioiTinh = model.GioiTinh,
+                    DiaChi = model.DiaChi,
+                    NgaySinh = model.NgaySinh,
+                    NgheNghiep = model.NgheNghiep,
+                    NgayTao = model.NgayTao,
+                    CCCD = model.CCCD,
+                    FullName = model.FullName
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    // Gán quyền "NhanVien" cho tài khoản người dùng
+                    await _userManager.AddToRoleAsync(user.Id, "NhanVien");
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
+            }
+
+            return View(model);
+        }
+
+
+        // GET: Admin/QLNhanVien/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // POST: Admin/QLNhanVien/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,GioiTinh,DiaChi,Trangthai,Ngaysinh,NgheNghiep,NgayTao,Bangcap,CCCD,FullName")] AspNetUser aspNetUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspNetUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aspNetUser);
+        }
+
+        // GET: Admin/QLNhanVien/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // POST: Admin/QLNhanVien/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            db.AspNetUsers.Remove(aspNetUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
